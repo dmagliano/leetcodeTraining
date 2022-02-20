e9205a7ad98ac9c78be372ecff05ccf2ecf72052
Diogo Magliano <diogo.magliano@gmail.com>
2022-02-19 23:40:40 -0300

    solução usando hashmap para indexar os valores em uma unica passada.

--- TwoSum.java
+++ TwoSum.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 class Main {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] nums = {3,2,4};
-        int target = 6;
+        int[] nums = {-3,2,3,4};
+        int target = 0;
         int[] answer = solution.twoSum(nums, target);
         System.out.println("["+answer[0]+","+answer[1]+"]");
     }
@@ -17,24 +18,17 @@ class Main {
 class Solution {
     public static int[] twoSum(int[] nums, int target) {
         int[] indexNums = new int[2];
-        for (int j = 0; j < nums.length; j++) {
-            if(nums[j] > target){
-                continue;
+        HashMap<Integer, Integer> indexes = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if (indexes.containsKey(difference)){
+                indexNums[0] = indexes.get(difference);
+                indexNums[1] = i;
+                break;
             }
-            for (int i = j+1; i < nums.length; i++) {
-                if(nums[i] > target){
-                    continue;
-                }
-                if(nums[i] > target - nums[j]){
-                    continue;
-                }
-                if(nums[i] == target - nums[j]){
-                    indexNums[1] = i;
-                    indexNums[0] = j;
-                }            
-            }
-        }   
-     
+            indexes.put(nums[i],i);
+        }
+
         return indexNums;
     }
 }
